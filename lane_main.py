7d0f035a8600805c0df5c40014234a11f2768eef
Sagar Singh <63928451+S4GAR@users.noreply.github.com>
2021-04-25 17:05:56 +0530

    Add files via upload
    This file contains all the image processing operations and till now it can identify the left lane. right lane need to be fixed

---
+++ lane_main.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot  as plt
+
+"""
+This is the unit where magic happens.
+"""
+
+#image = cv2.imread('road.jpg')
+
+def grayimage(image):
+    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+#lower_yellow = np.array([20,100,100])
+#upper_yellow = np.array([30,255,255])
+
+#mask_yellow = cv2.inRange(gray_image,lower_yellow,upper_yellow)
+
+def masking(gray_image):
+    mask_white = cv2.inRange(gray_image,170,255)
+
+    #mask_yw = cv2.bitwise_or(mask_white,mask_yellow)
+    mask_yw_image = cv2.bitwise_and(gray_image,mask_white)
+    return mask_yw_image
+
+def gaussian(mask_yw_image):
+    kernel_size = 5
+    gauss_gray = cv2.GaussianBlur(mask_yw_image,(5,5),kernel_size) 
+    return gauss_gray
+
+def canny(gauss_gray):
+    low_threshold = 50
+    high_threshold = 150
+    canny_edges = cv2.Canny(gauss_gray,low_threshold,high_threshold)
+    return canny_edges
+
+def roi(canny_img):
+    height = canny_img.shape[0]
+    width = canny_img.shape[1]
+    #print(height,width)
+    polygons = np.array([[(0,height-40),(550,height),(388,251)]])
+    mask = np.zeros_like(canny_img)
+    cv2.fillPoly(mask,polygons,255)
+    return cv2.bitwise_and(canny_img,mask)
+    
+def tlines(edges,image):
+    MinLineLength = 200
+    MaxLineGap = 10
+    #lines = cv2.HoughLinesP(masked_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+    lines = cv2.HoughLinesP(edges, 2, np.pi / 180,100,np.array([]),MinLineLength,MaxLineGap)
+    #print(lines)
+    for x1,y1,x2,y2 in lines[0]:
+        cv2.line(image,(x1,y1),(x2,y2),(0,255,0),8)
+        
+def writeimage(image):
+    cv2.imwrite("result.jpg",image)
+##########################
+
+
+
+
+"""
+gray_image = grayimage(image)
+mask_yw_image = masking(gray_image)
+gauss_gray = gaussian(mask_yw_image)
+canny_edges = canny(gauss_gray)
+section = roi(canny_edges)
+lines = tlines(section)
+writeimage();
+"""
+"""
+plt.imshow(canny_edges)
+plt.show()
+
+plt.imshow(section)
+plt.show()
+
+plt.imshow(image)
+plt.show()
+"""
